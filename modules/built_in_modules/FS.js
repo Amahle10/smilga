ce62014e172e392d4ca94bffbfd36cfd53dbf396
amahle10 <acepten.b@gmail.com>
2026-06-02 10:19:08 +0200

    async and sync ways to read fs or work with fs

---
+++ modules/built_in_modules/FS.js
@@ -0,0 +1,21 @@
+
+// syncronous approach
+const  {readFileSync, writeFileSync} = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+console.log(first, second);
+
+writeFileSync('./content/result.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});  
+
+
+// asyncronous approach
+const  {readFile, writeFile} = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(result);
+});
