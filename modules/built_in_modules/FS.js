9e0bcff497db5083d8513eb3795714500eb0a860
amahle10 <acepten.b@gmail.com>
2026-06-02 10:32:20 +0200

    async implementation

--- modules/built_in_modules/FS.js
+++ modules/built_in_modules/FS.js
@@ -17,5 +17,19 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
         console.error(err);
         return;
     }
-    console.log(result);
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const second = result;
+        writeFile('./content/result.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'}, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log('Files written successfully.');
+        });
+    });
 });
